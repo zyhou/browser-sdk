4fd9fe7779e91419222ee74468ea9e0fb37cbf6a
cy-moi <congyao119@gmail.com>
2024-08-06 15:42:47 +0200

    Add compute select test coverage

--- developer-extension/src/panel/components/tabs/eventsTab/computeFacetState.ts
+++ developer-extension/src/panel/components/tabs/eventsTab/computeFacetState.ts
@@ -12,7 +12,8 @@ export function computeSelectionState(
   const childrenFacets = facet.values?.[facetValue]?.facets
 
   // we cannot know how many children in total there are, so we need to have facetRegistry
-  const children = childrenFacets && childrenFacets.flatMap((child) => facetRegistry.getFacetChildrenValues(child.path))
+  const children =
+    childrenFacets && childrenFacets.flatMap((child: Facet) => facetRegistry.getFacetChildrenValues(child.path))
   const filteredFacetValues = Object.values(facetValuesFilter.facetValues).flat()
   const isFiltering = !!Object.keys(facetValuesFilter.facetValues)
 
